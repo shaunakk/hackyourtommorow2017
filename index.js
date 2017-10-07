7dd3621daf7fa778136430cf9dfba3405134d31d
Shaunak Kale <epicshaunak@gmail.com>
2017-10-07 16:44:20 -0700

    hi

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/build');
+app.set('view engine', 'jsx');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ usbank.js
@@ -0,0 +1,50 @@
+var http = require('http');
+
+var mydata = {
+  "OperatingCompanyIdentifier": "815",
+  "ProductCode": "DDA",
+  "PrimaryIdentifier": "00000000000000822943114"
+};
+
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+
+function PostCode(codestring) {
+  // Build the post string from an object
+  var post_data = JSON.stringify(mydata);
+
+  // An object of options to indicate where to post to
+  var post_options = {
+    host: 'api119521live.gateway.akana.com',
+    port: '80',
+    path: '/api/v1/account/details',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(post_data)
+    }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+    console.log("This is statusCode" + res.statusCode);
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      console.log('Response: ' + chunk);
+      var parsedJson = JSON.parse(chunk, null);
+    });
+    res.on('error', function(e) {
+      console.log('Error Response: ' + chunk);
+    });
+  });
+
+
+
+  // post the data
+  post_req.write(post_data);
+
+  post_req.end();
+}
+
+PostCode(JSON.stringify(mydata));
